8f784b06cc9899680a4efb095a9a68ea5d205f82
chanok <Chanok_boon@hotmail.com>
2021-07-06 12:25:23 +0700

    :sparkles: chatting feature

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require("express");
-
+var socket = require("socket.io");
 //App setup
 var app = express();
 var server = app.listen(4000, function () {
@@ -8,3 +8,14 @@ var server = app.listen(4000, function () {
 
 //static files
 app.use(express.static("public"));
+
+//Socket setup
+var io = socket(server);
+io.on("connection", function (socket) {
+  console.log("made socket connection", socket.id);
+
+  socket.on("chat", function (data) {
+    console.log(data.message);
+    io.sockets.emit("chat", data);
+  });
+});
